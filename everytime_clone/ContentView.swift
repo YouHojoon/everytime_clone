3def67fe8a4b79480777ec3c678f933d5bd63190
YouHojoon <dbghwns66@daum.net>
2022-03-16 21:10:16 +0900

    Initial Commit

---
+++ everytime_clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  everytime_clone
+//
+//  Created by 유호준 on 2022/03/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ everytime_clone/everytime_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  everytime_cloneApp.swift
+//  everytime_clone
+//
+//  Created by 유호준 on 2022/03/16.
+//
+
+import SwiftUI
+
+@main
+struct everytime_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
